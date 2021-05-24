96c7b45210d020f2216a9fd50b4de53631a83807
Adam2 <43184258+adamaka47@users.noreply.github.com>
2021-05-24 20:42:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,1 @@
+"use strict";$(".license-slider").slick({slidesToShow:3,arrows:!1,dots:!1}),$(".btn-prev").on("click",function(){$(".license-slider").slick("slickPrev")}),$(".btn-next").on("click",function(){$(".license-slider").slick("slickNext")}),$('[data-fancy="license"]').fancybox({});
